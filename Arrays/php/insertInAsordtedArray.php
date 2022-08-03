e7639be0baa8198aeeb9dc0a3825bdb0c0dcdf73
Zinah <zihna-19@hotmail.com>
2022-08-03 09:56:39 +0300

    adding insert in a sorted array algorithm

---
+++ Arrays/php/insertInAsordtedArray.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Insert and item into a sorted array
+ */
+function insertInAsortedArray(&$arr, $item)
+{
+   
+
+}
+
+//Driver code
+$arr = [1, 4, 5, 7, 10];
+insertInAsortedArray($arr, 2);
+print_r($arr);
+insertInAsortedArray($arr, 8);
+print_r($arr);
+?>
