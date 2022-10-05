fa676eca54060b38bc469f28ce23d9345d5faecd
Zinah <zihna-19@hotmail.com>
2022-10-05 08:33:27 +0300

    implementing search function for the the doublylinked list

---
+++ linkedlists/php/doublyLinkedList.php
@@ -0,0 +1,120 @@
+<?php
+
+class Node
+{
+    public $data;
+    public $next;
+    public $prev;
+
+    function __construct($data)
+    {
+        $this->data = $data;
+    }
+}
+class DoublyLinkedList
+{
+    public $head;
+    public $tail;
+
+
+    function __construct()
+    {
+        $this->head = null;
+        $this->tail = null;
+    }
+
+    function isEmpty()
+    {
+        return is_null($this->head);
+    }
+
+    function size()
+    {
+        $current = $this->head;
+        $count = 1;
+
+        while ($current != $this->tail) {
+            $count++;
+            $current = $current->next;
+        }
+
+        return $count;
+    }
+
+    function add($data)
+    {
+        $node = new Node($data);
+        if ($this->isEmpty()) {
+            $this->head = $node;
+            $this->tail = $node;
+            return;
+        }
+
+        $current = $this->head;
+        $node->next = $current;
+        $current->prev = $node;
+        $this->head = $node;
+
+    }
+
+    function append($data)
+    {
+        $node = new Node($data);
+        if ($this->isEmpty()) {
+            $this->head = $node;
+            $this->tail = $node;
+            return;
+        }
+
+        $current = $this->tail;
+        $current->next = $node;
+        $node->prev = $current;
+        $this->tail = $node;
+    }
+
+    function search($target)
+    {
+
+        $current =  $this->head;
+        if( $this->tail->data == $target) {
+            return true;
+        }
+        while($current != $this->tail) {
+            if($current->data == $target) {
+                return true;
+            }
+            $current = $current->next;
+        }
+        return false;
+
+}
+    function remove()
+    {
+
+    }
+    function nodeAtIndex($index)
+    {
+    }
+
+    function removeAtIndex($index)
+    {
+
+    }
+}
+
+
+/** Driver code */
+$doublyLinkedList = new DoublyLinkedList();
+$doublyLinkedList->add(10);
+$doublyLinkedList->add(20);
+$doublyLinkedList->append(30);
+
+// var_dump($doublyLinkedList->isEmpty());
+// var_dump($doublyLinkedList->size());
+// var_dump($doublyLinkedList->search(30));
+// var_dump($doublyLinkedList->search(10));
+// var_dump($doublyLinkedList->search(20));
+// var_dump($doublyLinkedList->search(50));
+// var_dump($doublyLinkedList->search(0));
+
+print_r($doublyLinkedList);
